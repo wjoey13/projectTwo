c95b01e3e82b40f82d8e2c0a664dded5d3abd129
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-08-28 12:57:15 -0600

    cleaned up my code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var animeCover = document.querySelector('.animeCover');
 var animeInfo = document.querySelector('#animeInfo');
 var animeVid = document.querySelector('.animeVid');
 
+
 axios.get("https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0").then(function(result){
     var aT = [];
     var aTPic = [];
@@ -13,20 +14,16 @@ axios.get("https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0").then(
         aTPic.push(result.data['data'][i]['attributes']['posterImage']['small']);
         aI.push(result.data['data'][i]['attributes']['synopsis']);
         aTube.push(result.data['data'][i]['attributes']['youtubeVideoId'])
-  }
- 
-  console.log(aTube)
+    }
   assignDropDown(aT)
-
+ 
     animeTitles.addEventListener("change", function(){
         animeCover.setAttribute('src', aTPic[animeTitles.value]);
         animeInfo.innerText = (aI[animeTitles.value]);
         animeVid.setAttribute('src', 'https://www.youtube.com/embed/'  + aTube[animeTitles.value]);
-        console.log(animeVid)
+       
     })
 }) 
-
-
 
 function assignDropDown(aT){
     for(let i =0; i < aT.length; i++){
@@ -34,5 +31,15 @@ function assignDropDown(aT){
         choice.setAttribute('label', aT[i])
         choice.setAttribute('value', [i])
         animeTitles.appendChild(choice)
+        var favTitle = choice.label
     }
+    
 }
+
+var button = document.querySelector('button')
+    button.addEventListener('click', function(e){
+        var local = document.querySelector('.animeTitles')
+        localStorage.setItem('Your Fav Anime', animeTitles.value)
+    })
+
+
