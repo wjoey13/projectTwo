cbb32494dd4d9101bfb429af74756770c03237b5
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-09-06 14:19:09 -0600

    added a branch and changed to fetch

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
+const apiUrl = "https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0"
 var animeTitles = document.querySelector('.animeTitles');
-var animeCover = document.querySelector('.animeCover');
 var animeInfo = document.querySelector('#animeInfo');
-var animeVid = document.querySelector('.animeVid');
+var animeVideo = document.querySelector('.animeVid');
+var animeCover = document.querySelector('.animeCover');
 
-
-axios.get("https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0").then(function(result){
-    var aT = [];
-    var aTPic = [];
-    var aI = [];
-    var aTube = [];
-    for (let i = 0; i < result.data['data'].length; i++) {
-        aT.push(result.data['data'][i]['attributes']['canonicalTitle']);
-        aTPic.push(result.data['data'][i]['attributes']['posterImage']['small']);
-        aI.push(result.data['data'][i]['attributes']['synopsis']);
-        aTube.push(result.data['data'][i]['attributes']['youtubeVideoId'])
+fetch(apiUrl)
+.then(function(response){
+    return response.json()
+    
+})
+.then(function(myJson){
+    console.log(myJson)
+    for (let i = 0; i < myJson['data'].length; i++) {
+        animeTitle.push(myJson['data'][i]['attributes']['canonicalTitle']);
+        animeTitlePic.push(myJson['data'][i]['attributes']['posterImage']['small']);
+        animeInfo.push(myJson['data'][i]['attributes']['synopsis']);
+        animePreview.push(myJson['data'][i]['attributes']['youtubeVideoId']);
     }
-  assignDropDown(aT)
- 
+    assignDropDown(animeTitle)
+    
     animeTitles.addEventListener("change", function(){
-        animeCover.setAttribute('src', aTPic[animeTitles.value]);
-        animeInfo.innerText = (aI[animeTitles.value]);
-        animeVid.setAttribute('src', 'https://www.youtube.com/embed/'  + aTube[animeTitles.value]);
-       
+        animeCover.setAttribute('src', animeTitlePic[animeTitles.value]);
+        animeInfo.innerText = (animeInfo[animeTitles.value]);
+        animeVideo.setAttribute('src', 'https://www.youtube.com/embed/'  + animePreview[animeTitles.value]);
+        
     })
 }) 
 
-function assignDropDown(aT){
-    for(let i =0; i < aT.length; i++){
+var animeTitle = [];
+var animeTitlePic = [];
+var animeInfo = [];
+var animePreview = [];
+
+function assignDropDown(animeTitle){
+    for(let i =0; i < animeTitle.length; i++){
         let choice = document.createElement('option');
-        choice.setAttribute('label', aT[i])
+        choice.setAttribute('label', animeTitle[i])
         choice.setAttribute('value', [i])
         animeTitles.appendChild(choice)
         var favTitle = choice.label
@@ -38,8 +45,9 @@ function assignDropDown(aT){
 
 var button = document.querySelector('button')
     button.addEventListener('click', function(e){
-        var local = document.querySelector('.animeTitles')
+        var localStorage = document.querySelector('.animeTitles')
         localStorage.setItem('Your Fav Anime', animeTitles.value)
     })
 
 
+console.log(animeTitlePic)
